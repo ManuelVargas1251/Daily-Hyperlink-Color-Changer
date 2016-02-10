856ff882141457750220aab3af22ec02cc3c6682
Manuel Vargas <manuelvargas@my.unt.edu>
2016-02-09 20:57:06 -0600

    Create linkcolorchanger.js

---
+++ linkcolorchanger.js
@@ -0,0 +1,50 @@
+var d = new Date();
+var n = d.getDay();
+  
+if (n == 0) {
+  document.linkColor = "#cc334d";
+    }
+if (n == 1) {
+  //document.linkColor = "#ad33cc";
+  var elements = document.querySelectorAll('a');
+  [].slice.call(elements).forEach(function(elem){
+    elem.style.color = '#ad33cc';
+  });
+  }
+
+if (n == 2) {
+  //document.linkColor = "#337fcc";
+  var elements = document.querySelectorAll('a');
+  [].slice.call(elements).forEach(function(elem){
+    elem.style.color = '#337fcc';
+  });
+  }
+
+if (n == 3) {
+  //document.linkColor = "#33ccc3";
+  var elements = document.querySelectorAll('a');
+  [].slice.call(elements).forEach(function(elem){
+    elem.style.color = '#33ccc3';
+  });
+  }
+if (n == 4) {
+  //document.linkColor = "#33cc57";
+  var elements = document.querySelectorAll('a');
+  [].slice.call(elements).forEach(function(elem){
+    elem.style.color = '#33cc57';
+  });
+  }
+if  (n == 5) {
+  //document.linkColor = "#c7cc33";
+  var elements = document.querySelectorAll('a');
+  [].slice.call(elements).forEach(function(elem){
+    elem.style.color = '#c7cc33';
+  });
+  }
+if (n == 6) {
+  //document.linkColor = "#cc5733";
+  var elements = document.querySelectorAll('a');
+  [].slice.call(elements).forEach(function(elem){
+    elem.style.color = '#cc5733';
+  });
+  }
