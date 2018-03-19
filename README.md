9b57c35090deae9770008b2d54a49fdc08115d5c
Manuel Vargas <manuelvargas@my.unt.edu>
2018-03-19 06:51:19 -0500

    1.1 🤢
    condensed js. updated example page

--- README.md
+++ README.md
@@ -1,30 +1,18 @@
 <!-- using shields.io for status buttons -->
 ![Programming language](https://img.shields.io/badge/Language-JavaScript-black.svg)
-![Build Status](https://img.shields.io/badge/Build-Passing-brightgreen.svg)
-![Version](https://img.shields.io/badge/Version-v1-blue.svg)
+![Version](https://img.shields.io/badge/Version-1.1-blue.svg)
 
 
 # Daily Hyperlink Color Changer
-Changes the color of your hyperlinks depending on what day of the week it is.
+Changes the color of a page's linked text depending on what day of the week it is.
 
-## getDay()
-Using getDay() I was able to test what day it was and apply a different color for each day
-
-| Getday() |  0 | 1 | 2 | 3 | 4 | 5 | 6 | 
-| --- | --- | --- | --- | --- | --- | --- | --- |
-| Day | Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday |
-| Color | `#cc334d` | `#ad33cc` | `#337fcc` | `#33ccc3` | `#33cc57` | `#f32` | `#cc5733` | 
-
-After it's found the current date, it will then find all elements with the 'a' hyperlink code and replace the default color with the one specified in the if statement.
 
 ## How To Use
-save .js to project and paste what's below to the html file:
+Download js file to project and include in HTML page
 
-```
+```html
 <script src="colorchange.js"></script>
 ```
 Make sure you paste this code lower in the html page like the footer so that all the links will have already loaded before script runs.
 
-You can view a demo on my main site: [mnl.space](http://mnl.space/Daily-Hyperlink-Color-Changer/)
-
-Or you can view my pen on [codepen.io](http://codepen.io/manuelvargas1251/pen/JGmZOL)
+This repo includes a demo `index.html` using the script. You can view a demo on my main site: [mnl.space](http://mnl.space/Daily-Hyperlink-Color-Changer/). Or you can view my pen on [codepen.io](http://codepen.io/manuelvargas1251/pen/JGmZOL)
--- colorchange.js
+++ colorchange.js
@@ -1,55 +1,20 @@
-//defines 'n' as value of the day
-var d = new Date();
-var n = d.getDay();
-  
-if (n == 0) {
-  //document.linkColor = "#cc334d";
-  var elements = document.querySelectorAll('a');  //find all elements with 'a'
-  [].slice.call(elements).forEach(function(elem){ //for loop of sorts
-    elem.style.color = '#cc334d'; //changes color
-  });
-    }
-if (n == 1) {
-  //document.linkColor = "#ad33cc";
-  var elements = document.querySelectorAll('a');
-  [].slice.call(elements).forEach(function(elem){
-    elem.style.color = '#ad33cc';
-  });
-  }
+//gets the number value of the day of the week; 0 through 6
+let currentDay = new Date().getDay()
 
-if (n == 2) {
-  //document.linkColor = "#337fcc";
-  var elements = document.querySelectorAll('a');
-  [].slice.call(elements).forEach(function(elem){
-    elem.style.color = '#337fcc';
-  });
-  }
+//find all elements with link tag, 'a'
+let elements = document.querySelectorAll('a')
 
-if (n == 3) {
-  //document.linkColor = "#33ccc3";
-  var elements = document.querySelectorAll('a');
-  [].slice.call(elements).forEach(function(elem){
-    elem.style.color = '#33ccc3';
-  });
-  }
-if (n == 4) {
-  //document.linkColor = "#33cc57";
-  var elements = document.querySelectorAll('a');
-  [].slice.call(elements).forEach(function(elem){
-    elem.style.color = '#33cc57';
-  });
-  }
-if  (n == 5) {
-  //document.linkColor = "#c7cc33";
-  var elements = document.querySelectorAll('a');
-  [].slice.call(elements).forEach(function(elem){
-    elem.style.color = '#c7cc33';
-  });
-  }
-if (n == 6) {
-  //document.linkColor = "#cc5733";
-  var elements = document.querySelectorAll('a');
-  [].slice.call(elements).forEach(function(elem){
-    elem.style.color = '#cc5733';
-  });
-  }
+let dayColors = [
+  '#cc334d',  //sunday
+  '#ad33cc',  //monday
+  '#337fcc',  //tuesday
+  '#33ccc3',  //wednesday
+  '#33cc57',  //thursday
+  '#c7cc33',  //friday
+  '#cc5733'   //saturday
+];  //necessary semi ☹
+
+//changes color for each link tag on page
+[].slice.call(elements).forEach(function (e) {
+  e.style.color = dayColors[currentDay]
+})
